2a8b7dc03c3d9e9d6127d470217458f926e5724e
teruh <zachclarkbyte@gmail.com>
2019-11-25 03:59:44 -0500

    Add login functionality

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<?php include ( __DIR__ . '/scripts/userLogin.php' ); ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,22 +8,33 @@
 	<link href="styles/register.css" rel="stylesheet">
 </head>
 <body class="text-center">
-	<form class="form-register">
-		<h1 class="text-white">Login</h1>
+	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="form-register">
+		<?php 
+			if(strlen($errorHTML) > 1) {
+				echo($errorHTML); 
+			}
+			if(strlen($infoHTML) > 1) {
+				echo($infoHTML); 
+			}
+		?>	
+		<fieldset>
+			<legend class="text-white">Login</legend>
+			<p class="text-white"><i>Username is not case-sensitive.</i></p>
 
-		<div class="form-group">
-			<label for="username" class="sr-only">Username</label>
-			<input type="email" id="username" class="form-control" placeholder="Username" required autofocus>
-		</div>
+			<div class="form-group">
+				<label for="username" class="sr-only">Username</label>
+				<input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
+			</div>
 
-		<div class="form-group">
-			<label for="password" class="sr-only">Password</label>
-			<input type="password" id="password" class="form-control" placeholder="Password" required>
-		</div>
+			<div class="form-group">
+				<label for="password" class="sr-only">Password</label>
+				<input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+			</div>
 
-		<div class="form-group">
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Log In</button>
-		</div>
+			<div class="form-group">
+				<input class="btn btn-lg btn-primary btn-block" type="submit" value="Login">
+			</div>
+		</fieldset>
 	</form>
 </body>
 </html>
